3e44de621a0b80e242bddc370ba0e7d393ab59ee
sebathegrey <sebastian.przybyl93@gmail.com>
2017-02-17 03:55:36 -0800

    Pierwszy commit z ubuntu

---
+++ myapp/obrazkowo/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/contact'
+
+  get 'home/faq'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
